79aa6237630f5904e5eeb9ef06ae6a43ebd8b962
simon <simonkhartoum@gmail.com>
2023-05-21 22:55:51 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user'
+class Student < User
 
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(added)
+        @knowledge << added
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
